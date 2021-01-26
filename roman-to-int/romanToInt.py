038f49e93d89f6efc5f196128b7023bb5127a50e
Justin Lohner <lohner4@gmail.com>
2021-01-25 17:17:54 -0700

    added solution to roman-to-int.py file for roman-to-int challenge

---
+++ roman-to-int/romanToInt.py
@@ -0,0 +1,21 @@
+class Solution:
+	def romanToInt(self, s: str) -> int:
+		nums = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000
+		}
+		x = 0
+		for i in range(len(s)):
+			if i < (len(s) - 1):
+				if nums[s[i]] < nums[s[i + 1]]:
+					x -= nums[s[i]]
+				else:
+					x += nums[s[i]]
+			else:
+				x += nums[s[i]]
+		return x
